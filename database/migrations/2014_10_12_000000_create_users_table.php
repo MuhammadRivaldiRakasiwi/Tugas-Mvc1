cb025f5a7d3b9f79fd964cbdde613e95d7946846
MuhammadRivaldiRakasiwi <rivaldi250rr@gmail.com>
2021-08-14 13:32:47 +0700

    users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        $table->id();
+           $table->string('name');
+           $table->string('email')->unique();
+           $table->timestamp('email_verified_at')->nullable();
+           $table->string('password');
+           $table->enum('role', ['admin','tu','hubin', 'siswa','kaprog','waka'])->default('siswa');
+           $table->rememberToken();
+           $table->timestamps();
     }
 
     /**
