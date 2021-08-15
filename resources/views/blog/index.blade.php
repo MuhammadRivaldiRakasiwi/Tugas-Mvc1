572c24b21516ffc68d48f3008467f50a4bced27c
MuhammadRivaldiRakasiwi <rivaldi250rr@gmail.com>
2021-08-15 17:44:36 +0700

    update login logout

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,11 +3,13 @@
 @section('content')
 
 @if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
+</div>
 @endif
 
+@if(Auth::check())
+@if(Auth::user()->role == 'admin')
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
     <a href="{{ route('blog.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
@@ -32,7 +34,7 @@
                 <img src="{{ url('/image/'.$blog->gambar) }}" class="rounded" style="width: 150px">
             </td>
             <td class="text-center">{{ $blog->judul }}</td>
-            <td >{!! $blog->content !!}</td>
+            <td>{!! $blog->content !!}</td>
 
             <td class="text-center">
                 <form action="{{ route('blog.destroy',$blog->id) }}" method="POST">
@@ -53,5 +55,21 @@
         @endforelse
     </table>
 </div>
+@endif
 
+@if(Auth::user()->role == 'hubin')
+<!-- <li class="nav-item">
+    <a class="nav-link" href="{{url('hubin')}}">Halaman Hubin</a>
+</li> -->
+<h1>Ini Halaman Hubin</h1>
+@endif
+
+@if(Auth::user()->role == 'siswa')
+<!-- <li class="nav-item">
+    <a class="nav-link" href="{{url('siswa')}}">Halaman Siswa</a>
+</li> -->
+<h1>INI HALAMAN SISWA</h1>
+@endif
+
+@endif
 @endsection
